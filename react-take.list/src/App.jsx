eaccbdc8646427036c94b1f1e394456642c491dd
yiseth <yicadi_1999@hotmail.com>
2023-12-19 18:36:31 +0100

    lista react

---
+++ react-take.list/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import "./App.css";
+
+
+
+function App() {
+  const { 
+
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleCompleteTodo,
+    handleUpdateTodo,
+  } = useTodo();
+
+  
+
+  return (
+  
+      <>
+        <div className="card-to-do">
+          <h1>Lista de tareas</h1>
+          <div className="counter-todos">
+            <h3>
+              N° Tareas: <span>{todosCount}</span>
+            </h3>
+            <h3>
+              Pendientes: <span>{pendingTodosCount}</span>
+            </h3>
+          </div>
+
+          <div className="add-todo">
+            <h3>Agregar Tarea</h3>
+            <Task handleNewTodo={handleNewTodo} />
+          </div>
+
+          <TaskList
+            todos={todos}
+            handleUpdateTodo={handleUpdateTodo}
+            handleDeleteTodo={handleDeleteTodo}
+            handleCompleteTodo={handleCompleteTodo}
+          />
+        </div>
+      </>
+      ); 
+  }
+
+
+export default App;
